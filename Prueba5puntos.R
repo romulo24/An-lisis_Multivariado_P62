afa1695a9b5fee4559fd4fa89c01f24d506beb80
Rómulo Jesús Torres Llivipuma <71521545+romulo24@users.noreply.github.com>
2023-05-21 16:04:28 -0500

    Update Prueba5puntos.R

--- Prueba5puntos.R
+++ Prueba5puntos.R
@@ -2,16 +2,21 @@
 library(readr)
 data <- read_csv("C:/Users/Usuario/Downloads/Cancer_Data.csv")
 View(data)
+
 # 1) Determinar cuál es el número de instancias del conjunto de datos
 # El dataset cuenta con 568 instancias
+
 # 2) Determinar cuántas personas tienen tumores malignos y benignos, respectivamente
 malignos = sum(data$diagnosis == "M") # Existen 212 personas con Cáncer Maligno
 benignos = sum(data$diagnosis == "B") # Existen 356 personas con Cáncer Benigno
+
 # 3)Crear un nuevo dataset que elimine la variable ID y Diagnosis. 
 # Todos los literales siguientes se basan en este nuevo dataset
 new = as.data.frame(data[,-(1:2)]); # Nuevo DataSet
+
 # 4) Generar un gráfico de pares entre todas las variables
 pairs(new)
+
 # 5) Determinar el grado de correlación entre todas las variables y graficar
 c = cor(new)
 corrplot(c)
